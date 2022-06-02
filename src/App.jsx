38f62149f12230def094f85766793244c0229dff
El-HobbitCR <jefh15@hotmail.com>
2022-06-02 14:46:54 -0600

    Creo mis dos componentes Navbar.jsx y Login.jsx, ademas corrijo la version de mi react-router-dom a --->react-router-dom@5.2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 // importo mi componente
 import Pokemones from "./components/Pokemones";
 
+// para poder usar rutas
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+// importo mis componentes
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+
+
 function App() {
 
+
+  // const store = generateStore()
+
+
+
   return (
-    <div className="container mt-3">
-      <Pokemones />
-    </div>
+    // para usar mis rutas
+    <Router>
+      <div className="container mt-3">
+        {/* creo mi componente Navbar */}
+        <Navbar />
+
+        {/* para poder cambiar las rutas */}
+        <Switch>
+          {/* AQUI VAN LAS RUTAS */}
+          {/* exact ---> para pagina principal */}
+          <Route
+            // component={Pokemones} ---> componente a usar
+            component={Pokemones}
+            path="/"
+            exact />
+          <Route component={Login} path="/login" exact />
+        </Switch>
+      </div >
+    </Router>
   );
 }
 
