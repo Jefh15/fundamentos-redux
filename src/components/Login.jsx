7b2b760e1541b954db5850fb081509adae27c79a
El-HobbitCR <jefh15@hotmail.com>
2022-06-02 15:49:21 -0600

    Creo el componente de Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Login = () => {
+
+
     return (
-        <div>Login</div>
+
+        <div className="mt-5 text-center">
+            <h3>Ingreso de usuarios</h3>
+            <hr />
+            {/* creo el boton */}
+            <button
+                className="btn btn-dark">
+                Ingresar con Google
+            </button>
+        </div>
     )
 }
 
