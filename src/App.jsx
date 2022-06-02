e24a61184990d1b37f1830791e08ea27fc2fec20
El-HobbitCR <jefh15@hotmail.com>
2022-06-02 14:11:25 -0600

    Muevo mi Provider React Redux a mi index.js que lo habia puesto mal en el App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 // importo mi componente
 import Pokemones from "./components/Pokemones";
 
-// importo mi provedor de redux
-import { Provider } from 'react-redux'
-// importo la store de redux
-import generateStore from './redux/store'
-
 function App() {
 
-  // hago la contante que me ayuda a poder generar y usar mis patos
-  const store = generateStore()
-
   return (
-    // {/* envuelvo mis componentes en un provider para poder usar esos archivos de redux */ }
-    // le envio al provedor el store(la tienda)
-    < Provider store={store}>
-      <div className="container mt-3">
-        <Pokemones />
-      </div>
-    </Provider >
+    <div className="container mt-3">
+      <Pokemones />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App.jsx';
 
 
+// importo mi provedor de redux
+import { Provider } from 'react-redux'
+// importo la store de redux
+import generateStore from './redux/store'
+
+
+// hago la contante que me ayuda a poder generar y usar mis patos
+const store = generateStore()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <App />
+  // {/* envuelvo mis componentes en un provider para poder usar esos archivos de redux */ }
+  // le envio al provedor el store(la tienda)
+  < Provider store={store}>
+    <App />
+  </Provider>
   // </React.StrictMode>
 );
 
