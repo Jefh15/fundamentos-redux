02a117dffb6fac1596e4a13a02049caacebb989b
El-HobbitCR <jefh15@hotmail.com>
2022-06-02 15:45:29 -0600

    Corrijo el Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// para poder hacer las rutas y redirigir
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
@@ -6,21 +7,27 @@ const Navbar = () => {
         <div className="navbar navbar-dark bg-dark">
             {/* para poder moverme a ese link, dando clic al nombre */}
             <Link
+                // envienos a la pagina de inicio
                 to="/"
                 className="navbar-brand">Poke API</Link>
 
             <div>
                 <div className="d-flex">
+                    {/* NavLink --> para pintar la clase active */}
                     <NavLink
                         className="btn btn-dark mr-2"
+                        // envianos al inicio o ruta raiz
                         to="/"
+                        // con el exact --> coloca el active cuando este en la ruta
                         exact
                     >
-                        Pokemon
+                        Inicio
                     </NavLink>
                     <NavLink
                         className="btn btn-dark mr-2"
+                        // envianos al login
                         to="/login"
+                        // con el exact --> coloca el active cuando este en la ruta
                         exact
                     >
                         Login
