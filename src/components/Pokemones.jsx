13fb41cf9468b35afd23dbe8385b11b4669a8846
El-HobbitCR <jefh15@hotmail.com>
2022-06-01 19:58:43 -0600

    Guardar API en mi localStorage() y leer del localStorage()

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux'
 
 // importamos la acción
 import {
+    anteriorPokemonAccion,
     obtenerPokemonesAccion,
     siguientePokemonAccion
 } from '../redux/pokeDucks'
@@ -21,34 +22,86 @@ const Pokemones = () => {
 
     // crearmos el state utilizando nuestra tienda
     // store.pokemones lo sacamos de la tienda
+    // AQUI TRABAJO LA LISTA DE POKEMONES
     const pokemones = useSelector(
         // devuelve una funcion de flecha a nuestro store
         // como queremos solo acceder al results --> results
         store => store.pokemones.results
     )
     // imprimo los el array de pokemones
-    console.log(pokemones)
+    // console.log(pokemones)
 
+    // para la paginacion, si existe el next
+    const next = useSelector(
+        // devuelve una funcion de flecha a nuestro store
+        // como queremos solo acceder al results --> results
+        store => store.pokemones.next
+    )
+
+    // para la paginacion, si existe previous
+    const previous = useSelector(
+        // devuelve una funcion de flecha a nuestro store
+        // como queremos solo acceder al results --> results
+        store => store.pokemones.previous
+    )
 
 
     return (
         <div className='container mt-2'>
             <h1>Pokemones!</h1>
-            {/* Me creo un boton */}
-            <button
-                className='btn btn-info'
-                // on click
-                onClick={
-                    // dispatch( llama la funcion especifica)
-                    () => dispatch(obtenerPokemonesAccion())}>Obtener pokemones</button>
-            {/* Me creo otro boton siguiente */}
-            <button
-                className='btn btn-info'
-                // on click
-                onClick={
-                    // dispatch( llama la funcion especifica)
-                    // siguientePokemonAccion(RECIBE UN NUMERO)
-                    () => dispatch(siguientePokemonAccion(20))}>Siguiente</button>
+
+            <br />
+
+
+            {/* HAGO UNA CONDICION PARA LOS BOTONES */}
+            {
+                // SI POKEMONES.lenght es igual a 0 ---> mostramos el boton de pokemones
+                pokemones.length === 0 && (
+                    // muestro el boton de obtener pokemones
+                    // {/* Me creo un boton */ }
+                    < button
+                        className='btn btn-warning mr-2'
+                        // on click
+                        onClick={
+                            // dispatch( llama la funcion especifica)
+                            () => dispatch(obtenerPokemonesAccion())}>Obtener pokemones</button>
+                )
+            }
+
+
+            {/* HAGO UNA CONDICION PARA EL BOTON DE NEXT */}
+            {
+                // SI EL NEXT NO ES NULL --> SI next existe pintemos el boton
+                next && (
+                    // {/* Me creo otro boton siguiente */ }
+                    < button
+                        className='btn btn-info mr-2'
+                        // on click
+                        onClick={
+                            // dispatch( llama la funcion especifica)
+                            // siguientePokemonAccion(RECIBE UN NUMERO)
+                            () => dispatch(siguientePokemonAccion())}>Siguiente →</button>
+                )
+
+            }
+
+            {/* HAGO UNA CONDICION PARA EL BOTON DE PREVIOUS */}
+            {
+                // SI EL PREVIOUS NO ES NULL --> SI previous existe pintemos el boton
+                previous && (
+                    // {/* Me creo otro boton anterior */ }
+                    < button
+                        className='btn btn-danger mr-2'
+                        // on click
+                        onClick={
+                            // dispatch( llama la funcion especifica)
+                            // anteriorPokemonAccion(RECIBE UN NUMERO)
+                            () => dispatch(anteriorPokemonAccion())}>← Anterior</button>
+                )
+            }
+
+
+
             {/* hago una lista */}
             <ul>
                 {
@@ -60,7 +113,7 @@ const Pokemones = () => {
                     ))
                 }
             </ul>
-        </div>
+        </div >
     )
 }
 
--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -28,6 +28,7 @@ const dataInitial = {
 // MIS TYPE
 const OBTENER_POKEMONES_EXITO = 'OBTENER_POKEMONES_EXITO'
 const SIGUIENTE_POKEMONES_EXITO = 'SIGUIENTE_POKEMONES_EXITO'
+const ANTERIOR_POKEMONES_EXITO = 'ANTERIOR_POKEMONES_EXITO'
 
 
 
@@ -46,7 +47,7 @@ export default function pokeReducer(state = dataInitial, action) {
             return {
                 // el state tiene que ser copiado de su data inicial
                 ...state,
-                // le decimos que ese array va a ser a la accion de mi payload
+                // le decimos que reemplace lo que haya cambiado
                 ...action.payload
             }
 
@@ -56,7 +57,17 @@ export default function pokeReducer(state = dataInitial, action) {
             return {
                 // el state tiene que ser copiado de su data inicial
                 ...state,
-                // le decimos que ese array va a ser a la accion de mi payload
+                // le decimos que reemplace lo que haya cambiado
+                ...action.payload
+            }
+
+        // boton de anterior
+        case ANTERIOR_POKEMONES_EXITO:
+            // abrimos un objeto
+            return {
+                // el state tiene que ser copiado de su data inicial
+                ...state,
+                // le decimos que reemplace lo que haya cambiado
                 ...action.payload
             }
 
@@ -89,7 +100,37 @@ export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
     // Hago destructuring para obtener la propiedad offset
     // const { offset } = getState().pokemones
 
+
+    // CUANDO OBTENEMOS POKEMONES POR PRIMERA VEZ, podemos preguntar si es necesario hacer la peticion de nuevo
+    // 'offset=0' ----> esta en mi key en mi localstorage
+    if (localStorage.getItem('offset=0')) {
+        // si eso existe
+        console.log('datos guardados')
+
+
+        // como ya tenemos la data simplemente hacemos el dispatch
+        // para poder activar nuestro switch ocupamos el dispath
+        dispatch(
+            // hago un objeto
+            {
+                // senalamos el type
+                type: OBTENER_POKEMONES_EXITO,
+                // tiene la respuesta
+                payload:
+                    // LO SACAMOS DE LOCALSTORAGE
+                    // COMO VIENE EN STRING ---> TENEMOS QUE TRASFORMARLO A ARRAY con ---> JSON.parse()
+                    JSON.parse(localStorage.getItem('offset=0'))
+
+            })
+
+        // HAGO EL RETURN para que salga del if
+        return
+    }
+
+
+
     try {
+        console.log('datos desde la api')
 
         // guardo la respuesta de la espera de axios
         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
@@ -112,6 +153,12 @@ export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
 
             })
 
+        // guardamos en localStorage el res
+        // como es un array y debo guardarlo en localStorage que solo permite string lo tranformo a string con JSON.stringify(propiedad)
+        // 'offset=0' ----> esta en mi key en mi localstorage
+        localStorage.setItem('offset=0', JSON.stringify(res.data))
+
+
 
     } catch (error) {
         console.log(error)
@@ -127,7 +174,7 @@ export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
 // dispatch -> activamos el reducer
 // getState -> obtenemos la data inicial en el state
 // como es un llamado de un api es async
-export const siguientePokemonAccion = (numero) => async (dispatch, getState) => {
+export const siguientePokemonAccion = () => async (dispatch, getState) => {
 
     // getState().pokemones ---> acccedemos a los pokemones 
     // console.log('getState', getState().pokemones.offset)
@@ -143,8 +190,37 @@ export const siguientePokemonAccion = (numero) => async (dispatch, getState) => 
     const { next } = getState().pokemones
     // console.log(next)
 
+
+    // next ----> esta en mi key en mi localstorage
+    if (localStorage.getItem(next)) {
+        // si eso existe
+        console.log('datos guardados')
+
+
+        // como ya tenemos la data simplemente hacemos el dispatch
+        // para poder activar nuestro switch ocupamos el dispath
+        dispatch(
+            // hago un objeto
+            {
+                // senalamos el type
+                type: OBTENER_POKEMONES_EXITO,
+                // tiene la respuesta
+                payload:
+                    // LO SACAMOS DE LOCALSTORAGE
+                    // COMO VIENE EN STRING ---> TENEMOS QUE TRASFORMARLO A ARRAY con ---> JSON.parse()
+                    JSON.parse(localStorage.getItem(next))
+
+            })
+
+        // HAGO EL RETURN para que salga del if
+        return
+    }
+
+
+
     try {
 
+        console.log('datos desde la api')
         // guardo la respuesta de la espera de axios
         // next es toda nuestra url
         const res = await axios.get(next)
@@ -170,6 +246,110 @@ export const siguientePokemonAccion = (numero) => async (dispatch, getState) => 
 
                 // }
             })
+
+        // guardamos en localStorage el res
+        // como es un array y debo guardarlo en localStorage que solo permite string lo tranformo a string con JSON.stringify(propiedad)
+        // next ----> esta en mi key en mi localstorage
+        localStorage.setItem(next, JSON.stringify(res.data))
+
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
+// ACCIONES
+// devuelve dos funciones de flecha
+// () ---> dentro van los paramtros
+// () ---> necesita un dispath y un getState
+// dispatch -> activamos el reducer
+// getState -> obtenemos la data inicial en el state
+// como es un llamado de un api es async
+export const anteriorPokemonAccion = () => async (dispatch, getState) => {
+
+    // getState().pokemones ---> acccedemos a los pokemones 
+    // console.log('getState', getState().pokemones.offset)
+
+    // accedemos al offset de mi estado initialState
+    // Hago destructuring para obtener la propiedad offset
+    // const { offset } = getState().pokemones
+    // // le sumo 20 a la peticion
+    // const siguiente = offset + numero
+
+
+    // accedo 
+    const { previous } = getState().pokemones
+    // console.log(previous)
+    // saco la cantidad de pokemones
+    // console.log(getState().pokemones.count)
+
+
+    // previous ----> esta en mi key en mi localstorage
+    if (localStorage.getItem(previous)) {
+        // if (localStorage.getItem(previous) && localStorage.getItem(previous).length === getState().pokemones) {
+        // si eso existe
+        console.log('datos guardados')
+
+
+        // como ya tenemos la data simplemente hacemos el dispatch
+        // para poder activar nuestro switch ocupamos el dispath
+        dispatch(
+            // hago un objeto
+            {
+                // senalamos el type
+                type: OBTENER_POKEMONES_EXITO,
+                // tiene la respuesta
+                payload:
+                    // LO SACAMOS DE LOCALSTORAGE
+                    // COMO VIENE EN STRING ---> TENEMOS QUE TRASFORMARLO A ARRAY con ---> JSON.parse()
+                    JSON.parse(localStorage.getItem(previous))
+
+
+            })
+
+        // HAGO EL RETURN para que salga del if
+        return
+    }
+
+    try {
+
+        console.log('datos desde la api')
+
+        // guardo la respuesta de la espera de axios
+        // next es toda nuestra url
+        const res = await axios.get(previous)
+        // para obtener el resultado
+        // res.data.results
+
+        // para poder activar nuestro switch ocupamos el dispath
+        dispatch(
+            // hago un objeto
+            {
+                // senalamos el type
+                type: ANTERIOR_POKEMONES_EXITO,
+                // tiene la respuesta
+                payload:
+                    // para obtener la lista de los pokemones
+                    // data lo proporcion axios
+                    res.data
+
+                // {
+                // ahora es un objeto en mi payload
+                // array: res.data.results,
+                // offset: siguiente
+
+                // }
+            })
+
+        // guardamos en localStorage el res
+        // como es un array y debo guardarlo en localStorage que solo permite string lo tranformo a string con JSON.stringify(propiedad)
+        // previous ----> esta en mi key en mi localstorage
+        localStorage.setItem(previous, JSON.stringify(res.data))
+
 
 
     } catch (error) {
